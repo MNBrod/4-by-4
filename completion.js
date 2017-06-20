ca1cc4374a068f774afc2e9ed29b6ae406124bc0
msnow90 <mattsnowis@yahoo.com>
2017-06-20 14:33:10 -0400

    Initial setup of completion

---
+++ completion.js
@@ -0,0 +1,24 @@
+// board.board contains cells
+// board.print for nice print
+var hintCoord = {
+  0: [0, 0],
+  1: [0, 1],
+  2: [0, 2],
+  3: [0, 3],
+  4: [0, 3],
+  5: [1, 3],
+  6: [2, 3],
+  7: [3, 3],
+  8: [3, 3],
+  9: [3, 2],
+  10: [3, 1],
+  11: [3, 0],
+  12: [3, 0],
+  13: [2, 0],
+  14: [1, 0],
+  15: [0, 0]
+}
+
+function isComplete(board, clues) {
+
+}
