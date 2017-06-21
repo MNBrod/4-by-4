5e84d9167fe64a1e9127f2995fc8f0582ea96ffa
Max Brodheim <max.brodheim@gmail.com>
2017-06-21 16:01:31 -0400

    ALMOST THERE

--- permAttack.js
+++ permAttack.js
@@ -1,17 +1,53 @@
+var combos = {};
+var rowPosibs = {
+    0: [],
+    1: [],
+    2: [],
+    3: []
+};
+// var hints1ans = [
+//     [2, 1, 4, 3],
+//     [3, 4, 1, 2],
+//     [4, 2, 3, 1],
+//     [1, 3, 2, 4]
+// ]
+// var hints2ans = [
+//     [1, 3, 4, 2],
+//     [4, 2, 1, 3],
+//     [3, 4, 2, 1],
+//     [2, 1, 3, 4]
+// ];
+// var hints2ansW = [
+//     [1, 2, 3, 4],
+//     [1, 2, 3, 4],
+//     [1, 2, 3, 4],
+//     [1, 2, 3, 4]
+
+// ];
+var hintArrs = {
+    top: [],
+    right: [],
+    bottom: [],
+    left: []
+}
+var hints1 = [0, 0, 1, 2, 0, 2, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0];
+var hints2 = [2, 2, 1, 3, 2, 2, 3, 1, 1, 2, 2, 3, 3, 2, 1, 3]
+var test = '1234';
+
 
 function permut(string) {
     if (string.length < 2) return string; // This is our break condition
 
     var permutations = []; // This array will hold our permutations
 
-    for (var i=0; i<string.length; i++) {
+    for (var i = 0; i < string.length; i++) {
         var char = string[i];
 
         // Cause we don't want any duplicates:
         if (string.indexOf(char) != i) // if char was used already
             continue;           // skip it this time
 
-        var remainingString = string.slice(0,i) + string.slice(i+1,string.length); //Note: you can concat Strings via '+' in JS
+        var remainingString = string.slice(0, i) + string.slice(i + 1, string.length); //Note: you can concat Strings via '+' in JS
 
         for (var subPermutation of permut(remainingString))
             permutations.push(char + subPermutation)
@@ -19,7 +55,6 @@ function permut(string) {
     }
     return permutations;
 }
-var test = '1234';
 
 var visible = function (str) {
     var arr = str.split('');
@@ -33,7 +68,7 @@ var visible = function (str) {
     }
     highest = 0;
     var Rviews = 0;
-    for (var i = arr.length-1; i > -1; i--) {
+    for (var i = arr.length - 1; i > -1; i--) {
         if (arr[i] > highest) {
             highest = arr[i];
             Rviews++;
@@ -42,35 +77,159 @@ var visible = function (str) {
     return [Lviews, Rviews];
 
 }
-var combos = {};
 
 var fillCombos = function (arr) {
     for (var i = 0; i < arr.length; i++) {
         var views = visible(arr[i]);
-        combos[arr[i]] = {right: views[0], left: views[1]};
+        combos[arr[i]] = { lookingRight: views[0], lookingLeft: views[1] };
     }
-console.log(combos);
+    //console.log(combos);
 }
 
-var rowPosibs = {
-    0: [],
-    1: [],
-    2: [],
-    3: []
-};
-
-var reduceWithHint = function (hint, com) {
-    var result = [];
-    var keys = Object.keys(com);
-    var vals = Object.values(com);
-    for (var i = 0; i < key.length; i++) {
-        if (vals[i].right === hint) {
-            result.push(vals[i].right)
+var reduceWithHint = function (hintArrs, combos) {
+    const keys = Object.keys(rowPosibs);
+    const cKeys = Object.keys(combos);
+    for (let i = 0; i < keys.length; i++) {
+        //console.log("Looking at row " + i);
+        var posibs = [];
+        //go through each row
+        var rHint = hintArrs.right[i];
+        var lHint = hintArrs.left[i];
+        //console.log("Right hint is ", rHint, ", Left hint is ", lHint);
+        for (let c = 0; c < cKeys.length; c++) {
+            if ((combos[cKeys[c]].lookingLeft === lHint || combos[cKeys[c]].lookingRight === rHint) || (lHint === 0 && rHint === 0)) {
+                posibs.push(cKeys[c]);
+            }
         }
+        rowPosibs[i] = posibs;
+    }
+    //console.log("\n\n");
+    for (let i = 0; i < 4; i++) {
+        //console.log("For row ", i, ", there are ", rowPosibs[i].length, " possibilites");
+    }
+}
+var splitHints = function (arr) {
+    hintArrs.top = arr.splice(0, 4);
+    hintArrs.right = arr.splice(0, 4);
+    hintArrs.bottom = reverseArr(arr.splice(0, 4));
+    hintArrs.left = reverseArr(arr.splice(0, 4));
+    //console.log("\n", hintArrs);
+}
+var reverseArr = function (arr) {
+    var temp = arr.slice();
+    var result = [];
+    for (var i = arr.length - 1; i > -1; i--) {
+        result.push(arr[i]);
     }
     return result;
 }
 
-// console.log(visible(test));
-var arr = permut('1234');
-fillCombos(arr);
+// var validateBoard = function (arr) {
+//     console.log(arr.join("\n"));
+//     arr = rotate(arr, 4);
+//     arr = rotate(arr, 4);
+//     arr = rotate(arr, 4);
+//     console.log("\n");
+//     console.log(arr.join("\n"));
+
+//     for (var i = 0; i < 4; i++) { //iterate through the 'rows'
+//         var lHint = hintArrs.bottom[i];
+//         var rHint = hintArrs.top[i];
+//         if (arr[i].sort() !== [1, 2, 3, 4]) return false;
+//         for (let c = 0; c < arr[i].length; c++) {
+//             var Lview = visible(arr[i].toString())[0]
+//             var Rview = visible(arr[i].toString())[1]
+//             if (!((rHint === Rview) || rHint === 0) && !((lHint === Lview) || lHint === 0)) {
+//                 return false;
+//             }
+//         }
+//     }
+//     return true;
+//     //convert the rows into coloumns
+//     //validate each coloumn
+//     //if theyre all valid, then they're done
+// }
+
+var validate = function (board) {
+    var colViews = [];
+    //console.log(board.join('\n'));
+    //console.log("\n");
+    board = rotate(board);
+    //console.log(board.join('\n'));
+    var cols = [
+        board[0],
+        board[1],
+        board[2],
+        board[3]
+    ];
+    for (var i = 0; i < cols.length; i++) {
+        var dup = cols[i].slice();
+        if (dup.sort().toString() !== '1,2,3,4') return false;
+        colViews = visible(cols[i].toString());
+        // console.log(cols[i],i, "|||||", colViews);
+        // console.log("\n");
+        // console.log(hintArrs.bottom[i], hintArrs.top[i]);
+        if (!isCompatible(colViews, [hintArrs.bottom[i], hintArrs.top[i]])) return false;
+    }
+    return true;
+}
+var isCompatible = function (views, hints) {
+    if ((views[0] !== hints[0]) && hints[0] !== 0) {
+        return false;
+    }
+    if ((views[1] !== hints[1]) && hints[0] !== 0) {
+        return false;
+    }
+    return true;
+}
+function rotate(matrix) {
+    var n = matrix.length;
+    for (var i = 0; i < n / 2; i++) {
+        for (var j = 0; j < Math.ceil(n / 2); j++) {
+            var temp = matrix[i][j];
+            matrix[i][j] = matrix[n - 1 - j][i];
+            matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+            matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+            matrix[j][n - 1 - i] = temp;
+        }
+    }
+    return matrix;
+}
+var bruteForce = function () {
+    for (var a = 0; a < rowPosibs[0].length; a++) {
+        for (var b = 0; b < rowPosibs[1].length; b++) {
+            for (var c = 0; c < rowPosibs[2].length; c++) {
+                for (var d = 0; d < rowPosibs[3].length; d++) {
+                    var iteration = [
+                        rowPosibs[0][a].split(''),
+                        rowPosibs[1][b].split(''),
+                        rowPosibs[2][c].split(''),
+                        rowPosibs[3][d].split(''),
+                    ]
+                    if (validate(iteration)) return iteration;
+                }
+            }
+        }
+    }
+}
+// // console.log(visible(test));
+// var arr = permut(test);
+// fillCombos(arr);
+// splitHints(hints1);
+// reduceWithHint(hintArrs, combos);
+
+// console.log("\n\n\n------------------");
+// console.log(
+// bruteForce()
+// );
+var solver = function (clues) {
+    splitHints(clues)
+    fillCombos(permut('1234'));
+    reduceWithHint(hintArrs, combos);
+    var ans = bruteForce();
+    rotate(ans);
+    rotate(ans);
+    rotate(ans);
+    return ans;
+    //console.log("finished: ", bruteForce());
+}
