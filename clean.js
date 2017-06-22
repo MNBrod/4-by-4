ed944402d1c56ddb978785a4cd4aa93224cf58b4
Max Brodheim <max.brodheim@gmail.com>
2017-06-22 17:21:26 -0400

    started testing

--- clean.js
+++ clean.js
@@ -1,17 +1,18 @@
-var val = function (board, hints) {
-  if (!validateHints(board, hints)) {
-    return false;
+function validateRows(board) {
+  //console.log(board.join('\n'));
+  for (let i = 0; i < board[0].length; i++) {
+    var temp = board[i].slice();
+    if (temp.sort().toString() != '1,2,3,4') {
+      //console.log("--> false");
+      return false;
+    }
   }
-    console.log("not called early at hints");
-  board = rotate(board);
-  console.log(board);
-  for (var i = 0; i < board.length; i++) {
-    //console.log("called", board[i]);
-    if (board[i].sort().toString() !== '1,2,3,4') return false;
-  }
+  //console.log("--> true");
   return true;
 }
-function rotate(matrix) {
+
+function rotate(board) {
+  var matrix = board.slice();
   //console.log(matrix.join('\n'));
   var n = matrix.length;
   for (var i = 0; i < n / 2; i++) {
@@ -27,41 +28,26 @@ function rotate(matrix) {
 }
 
 var validateHints = function (board, clues) {
-  var line = [];
-  var num;
-  var done = false;
   for (var i = 0; i < clues.length; i++) {
     if (clues[i] !== 0) {
-      if (i < 4) {
-        console.log("1");
-        line = [board[0][i], board[1][i], board[2][i], board[3][i]]
-      } else if (i < 8) {
-        console.log("2");
-        num = i - 4;
-        line = [board[num][3], board[num][2], board[num][1], board[num][0]];
-      } else if (i < 12) {
-        console.log("3");
-        num = 11 - i;
-        line = [board[3][num], board[2][num], board[1][num], board[0][num]]
-      } else {
-        console.log("4");
-        num = 15 - i;
-        line = [board[num][0], board[num][1], board[num][2], board[num][3]];
-      }
-      var highest = 0;
-      var views = 0;
-      for (var i = 0; i < line.length; i++) {
-        if (line[i] > highest) {
-          highest = line[i];
-          views++;
-        }
-      }
-      if (views !== clues[i])  done = true;
-      line = [];
+      let line = makeLine(board, i)
+      if (!validateLine(line, clues[i])) return false;
     }
   }
   return true;
 }
+
+var val = function (board, hints) {
+  if (!validateRows(board)) return false;
+  console.log("Passed rows counting");
+  var rotated = rotate(board);
+  if (!validateRows(rotated)) return false;
+  console.log("Passed cols counting");
+  if (!validateHints(board, hints)) return false;
+  console.log("Passed hint validation");
+  return true;
+}
+
 function permut(string) {
   if (string.length < 2) return string; // This is our break condition
   var permutations = []; // This array will hold our permutations
@@ -80,12 +66,12 @@ function permut(string) {
 var bruteForce = function (permutations, hints) {
   var i = 0;
   var length = permutations.length;
-  for (var a = 0; a < length; a++) {
-    for (var b = 0; b < length; b++) {
-      for (var c = 0; c < length; c++) {
-        for (var d = 0; d < length; d++) {
+  for (let a = 0; a < length; a++) {
+    for (let b = 0; b < length; b++) {
+      for (let c = 0; c < length; c++) {
+        for (let d = 0; d < length; d++) {
           //console.log(i++);
-          var iteration = [
+          let iteration = [
             permutations[a].split(''),
             permutations[b].split(''),
             permutations[c].split(''),
@@ -103,15 +89,74 @@ var bruteForce = function (permutations, hints) {
 }
 var solver = function (clues) {
   var permutations = permut('1234');
-  //splitHints(clues)
-  //fillCombos(permut('1234'));
-  //reduceWithHint(hintArrs, combos);
   var ans = bruteForce(permutations, clues);
-  //console.log(combos);
-  //rotate(ans);
-  //rotate(ans);
-  //rotate(ans);
   console.log("finished: ", ans);
-  //return ans;
 }
-module.exports = solver;
+/**
+ * Assumes:
+ *      line is left to right
+ *      hint is from the left
+ *      the correct hint is given for the correct line
+ *
+ * @param {array} line
+ * @param {number} hint
+ * @returns whether the line, read left to right, matches the hint given
+ */
+function validateLine(line, hint) {
+  let views = 0;
+  for (let i = 0, highest = 0; i < line.length; i++) {
+    if (line[i] > highest) {
+      highest = line[i];
+      views++;
+    }
+  }
+  if (views === hint) return true;
+  return false;
+}
+
+/**
+ * @param {2D array} board
+ * @param {number} i
+ * @returns a line starting at the hint, moving away from it
+ *     0 1 2 3
+ *     _ _ _ _
+ * 15 |_|_|_|_| 4
+ * 14 |_|_|_|_| 5
+ * 13 |_|_|_|_| 6
+ * 12 |_|_|_|_| 7
+ *    11   9
+ *       10  8
+ *
+ */
+function makeLine(board, i) {
+  var line = [];
+  var num = 0;
+  if (i < 4) {
+    //console.log("1");
+    line = [board[0][i], board[1][i], board[2][i], board[3][i]]
+  } else if (i < 8) {
+    //console.log("2");
+    num = i - 4;
+    line = [board[num][3], board[num][2], board[num][1], board[num][0]];
+  } else if (i < 12) {
+    // console.log("3");
+    num = 11 - i;
+    line = [board[3][num], board[2][num], board[1][num], board[0][num]]
+  } else {
+    // console.log("4");
+    num = 15 - i;
+    line = [board[num][0], board[num][1], board[num][2], board[num][3]];
+  }
+  return line;
+}
+module.exports = {
+  solver: solver,
+  makeLine: makeLine,
+  validateRows: validateRows,
+  validateHints: validateHints,
+  validateLine: validateLine,
+  permut: permut,
+  rotate: rotate,
+  val: val
+
+}
--- tests.js
+++ tests.js
@@ -1,11 +1,86 @@
 var solver = require('./clean.js');
+var chalk = require('chalk');
 
-var hint3 = [0,0,3,0,0,4,0,0,0,0,0,0,2,0,0,0]
+var hint3 = [0, 0, 3, 0, 0, 4, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0]
+var hintFake = [0, 0, 3, 0, 1, 4, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0]
+var hintFake2 = [0, 0, 3, 0, 1, 4, 0, 0, 0, 0, 1, 0, 2, 0, 0, 0]
+var hintAll = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 var ans3 = [
-  [3,2,1,4],
-  [4,3,2,1],
-  [2,1,4,3],
-  [1,4,3,2]
+  [3, 2, 1, 4],
+  [4, 3, 2, 1],
+  [2, 1, 4, 3],
+  [1, 4, 3, 2]
+];
+//solver(hint3);
+var makeLinesHor = [
+  [1, 2, 3, 4],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0]
+];
+var makeLinesVert = [
+  [1, 0, 0, 0],
+  [2, 0, 0, 0],
+  [3, 0, 0, 0],
+  [4, 0, 0, 0]
+]
+var validateRowsBoard = [
+  [3, 2, 1, 4],
+  [4, 3, 2, 1],
+  [2, 1, 4, 3],
+  [1, 4, 3, 2]
 ];
 
-solver(hint3);
+
+/**
+ *     0 1 2 3
+ *     _ _ _ _
+ * 15 |_|_|_|_| 4
+ * 14 |_|_|_|_| 5
+ * 13 |_|_|_|_| 6
+ * 12 |_|_|_|_| 7
+ *    11   9
+ *       10  8
+ */
+var tests = true;
+if (tests) {
+  console.log("\nTESTS:\n");
+  console.log(chalk.green("\nMake Line tests:"));
+  console.log(solver.makeLine(makeLinesHor, 4).toString() === '4,3,2,1');
+  console.log(solver.makeLine(makeLinesVert, 11).toString() === '4,3,2,1');
+  console.log(solver.makeLine(makeLinesHor, 15).toString() === '1,2,3,4');
+  console.log(solver.makeLine(makeLinesVert, 0).toString() === '1,2,3,4');
+
+  console.log(chalk.green("\nValidate Count:"));
+  console.log(solver.validateRows(validateRowsBoard));
+  console.log(solver.validateRows(solver.rotate(validateRowsBoard)));
+
+  console.log(chalk.green("\nValidate Hints:"));
+  console.log(solver.validateHints(ans3, hint3));
+  console.log(!solver.validateHints(ans3, hintFake2));
+  console.log(solver.validateHints(ans3, hintAll));
+  console.log(!solver.validateHints(ans3, hintFake));
+  console.log(!solver.validateHints(makeLinesHor, hint3));
+
+
+  console.log(chalk.green("\nVal:"));
+  console.log(solver.val(ans3, hint3));
+
+}
+// console.log(solver.rotate(validateRowsBoard).join('\n'));
+
+// solver.solver(hint3);
+
+
+  // console.log(solver.validateHints([1, 2, 3, 4], 4));
+  // console.log(solver.validateHints([2, 1, 3, 4], 3));
+  // console.log(solver.validateHints([3, 2, 1, 4], 2));
+  // console.log(solver.validateHints([4, 3, 2, 1], 1));
+  // console.log(solver.validateHints([2, 1, 4, 3], 2));
+  // console.log(solver.validateHints([1, 4, 3, 2], 2));
+  // console.log(solver.validateHints([4, 1, 3, 2], 2));
+  // //false ones
+  // console.log(!solver.validateHints([1, 4, 3, 2], 3));
+  // console.log(!solver.validateHints([1, 4, 3, 2], 3));
+  // console.log(!solver.validateHints([3, 4, 1, 2], 1));
+  // console.log(!solver.validateHints([1, 2, 4, 3], 2));
